c135dd6812ca53a04b45a6534746b744b0512077
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-12-10 23:06:16 -0300

    p

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ uploaded_files = st.file_uploader(
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    st.write(bytes_data + '\n')
