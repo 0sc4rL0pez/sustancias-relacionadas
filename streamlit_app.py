9657386a31608115087975b2a4f5b84413040191
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-12-10 23:09:26 -0300

    another way

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,24 @@ st.write(
     "App para realizar la identificacion de picos en cromatogramas"
 )
 
-uploaded_files = st.file_uploader(
-    "Choose a CSV file", accept_multiple_files=True
-)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
-    st.write('\n')
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
