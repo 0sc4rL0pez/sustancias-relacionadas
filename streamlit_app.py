b20930180b5275826bf82e4497b5005703b1a5d3
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-12-10 23:03:28 -0300

    upload code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("Sustancias Relacionadas")
 st.write(
     "App para realizar la identificacion de picos en cromatogramas"
 )
+
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
