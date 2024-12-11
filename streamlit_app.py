df2c315881d251810c9b845b88da7bd55e2aef63
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-12-10 23:20:21 -0300

    muestra

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,21 @@
 import streamlit as st
-
-st.title("Sustancias Relacionadas")
-st.write(
-    "App para realizar la identificacion de picos en cromatogramas"
-)
-
-import streamlit as st
 import pandas as pd
 from io import StringIO
+st.title("Sustancias Relacionadas")
 
-uploaded_file = st.file_uploader("Choose a file")
+
+st.write(
+    "Seleccionar archivos en formato CSV"
+)
+
+uploaded_file = st.file_uploader("Blanco:")
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file,sep=',')
-    st.write(dataframe)
+    dataframe_blanco = pd.read_csv(uploaded_file,sep=',')
+    st.write(dataframe_blanco)
+
+uploaded_file = st.file_uploader("Muestra:")
+if uploaded_file is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe_muestra = pd.read_csv(uploaded_file,sep=',')
+    st.write(dataframe_muestra)
