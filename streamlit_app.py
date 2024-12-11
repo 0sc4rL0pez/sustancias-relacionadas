a43f6c0e6b782443b99eca7f606007a6b3400594
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-12-10 23:01:00 -0300

    text inserted

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Sustancias Relacionadas")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "App para realizar la identificacion de picos en cromatogramas"
 )
