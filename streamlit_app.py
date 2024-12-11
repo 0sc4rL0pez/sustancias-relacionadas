ede3fcfc50fd486d4520f942b29faa199ffdc364
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-12-10 23:34:42 -0300

    ident

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,17 +14,17 @@ if uploaded_file is not None:
     dataframe_blanco = pd.read_csv(uploaded_file,sep=',')
     st.write(dataframe_blanco)
 
-uploaded_file = st.file_uploader("Muestra:")
-if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe_muestra = pd.read_csv(uploaded_file,sep=',')
-    st.write(dataframe_muestra)
+    uploaded_file = st.file_uploader("Muestra:")
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe_muestra = pd.read_csv(uploaded_file,sep=',')
+        st.write(dataframe_muestra)
 
-intervalo_por = st.number_input("Insertar intervalo de tiempo(%): ",value=0.5)
-area_multp = st.number_input("Insertar multiplicador área: ",value=2)
-col_time= st.number_input("Nro columna para tiempo ret")
-col_area = st.number_input("Nro columna para area")
+    intervalo_por = st.number_input("Insertar intervalo de tiempo(%): ",value=0.5)
+    area_multp = st.number_input("Insertar multiplicador área: ",value=2)
+    col_time= st.number_input("Nro columna para tiempo ret")
+    col_area = st.number_input("Nro columna para area")
 
-dataframe_blanco['esta'] = dataframe_blanco.iloc[:,col_time]
+    dataframe_blanco['esta'] = dataframe_blanco.iloc[:,col_time]
 
-st.write(dataframe_blanco['esta'])
+    st.write(dataframe_blanco['esta'])
