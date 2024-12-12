8a07595324181543cbc8c1eddc25c139e244696a
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-12-11 23:14:04 -0300

    reset

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,23 +24,20 @@ if uploaded_file is not None:
         area_multp = st.number_input("Insertar multiplicador área: ",value=2)
         col_time= int(st.number_input("Nro columna para tiempo ret",value=1))
         col_area = int(st.number_input("Nro columna para area",value=2))
-        dataframe_muestra['Pico_presente'] = False
+        dataframe_muestra['Pico_presente'] = ""
         dataframe_muestra['Var Time'] = np.inf
-        def areaCorrecta(fila):
-            return float(dataframe_blanco.iloc[fila,col_area])*2 >= float(dataframe_muestra.iloc[fila,col_area])
+        
 
-        def estaEntiempo(t):
-            contador = 0
-            for val in dataframe_muestra.iloc[:,col_time].tolist():
+        def estaEntiempo(t,j):
+            for i,val in enumerate(dataframe_muestra.iloc[:,col_time].tolist()):
                 val = float(val)
                 var_time = abs(100*(val-t))/t
                 if var_time<intervalo_tiempo:
-                    dataframe_muestra.loc[contador,"Var Time"] = min(var_time, dataframe_muestra["Var Time"][contador])
-                    if areaCorrecta(contador):
-                        dataframe_muestra.loc[contador,"Pico_presente"] = True
-                contador = contador + 1
+                    if float(dataframe_blanco.iloc[j,col_area])*2 >= float(dataframe_muestra.iloc[i,col_area]):
+                        dataframe_muestra.loc[i,"Name"] = "Blanco"
             
-        for val in dataframe_blanco.iloc[:,col_time].tolist():
-            estaEntiempo(float(val))
+        
+        for j,val in enumerate(dataframe_blanco.iloc[:,col_time].tolist()):
+            estaEntiempo(float(val),j)
 
         st.write(dataframe_muestra)
